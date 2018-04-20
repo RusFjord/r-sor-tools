39a207ba4d7d7bb642e892a67603fa8b217c71e3
Fedor Endovitskiy <rusfjord@ulteam8.ru>
2018-04-20 19:54:57 +0300

    Инициализация проекта

---
+++ src/main/java/ru/ulteam8/App.java
@@ -0,0 +1,13 @@
+package ru.ulteam8;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
